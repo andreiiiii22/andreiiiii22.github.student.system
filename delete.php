ba1b2baa7cd7d1dcb27eab471760a9a3a77f1f5a
andreiiiii22 <centenolanceandre@gmail.com>
2025-10-05 22:59:53 +0800

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,4 +1,17 @@
 <?php
 include 'db.php';
-?>
 
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    $delete = $conn->query("DELETE FROM students WHERE id='$id'");
+
+    if ($delete) {
+        header("Location: index.php?msg=deleted");
+    } else {
+        echo "Error deleting record: " . $conn->error;
+    }
+} else {
+    echo "No ID provided!";
+}
+?>
